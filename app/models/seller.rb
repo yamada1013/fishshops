2f82cd5609cc9042f2994cf8e73811e107d89b9d
Yuko Yamada <oly.oly.onlyno1@gmail.com>
2021-05-19 19:24:45 +0900

    コミット1

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,6 +1,8 @@
 class Seller < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
